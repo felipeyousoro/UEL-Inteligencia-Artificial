b7b43c94b8a5275ecce6b74ff162be754927bb72
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2023-01-25 02:08:45 -0300

    graficos feitos

--- Analise Exploratoria/main.py
+++ Analise Exploratoria/main.py
@@ -1,11 +1,19 @@
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
 
 CSV_PATH = 'Analise Exploratoria/pokemon_alopez247.csv'
+
 
 
 if __name__ == '__main__':
 
     data_pandas = pd.read_csv(CSV_PATH)
 
-    print(data_pandas[['HP', 'Attack', "Defense", "Sp_Atk", "Sp_Def", "Speed"]].mean())
+    columns_of_stats =  ['Total', 'HP', 'Attack', 
+                         'Defense', 'Sp_Atk', 'Sp_Def', 'Speed']
+
+    for column in columns_of_stats:
+        data_pandas[column].plot(kind='hist', title=column)
+        plt.savefig('Analise Exploratoria/{}.png'.format(column))
+        plt.close()
