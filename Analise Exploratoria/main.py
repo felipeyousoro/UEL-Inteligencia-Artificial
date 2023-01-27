bc59296e3ac4e62687e09903bc98a115e946d5bf
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2023-01-27 01:43:19 -0300

    passando pra grafico de caixa

--- Analise Exploratoria/main.py
+++ Analise Exploratoria/main.py
@@ -7,37 +7,37 @@ CSV_PATH = 'Analise Exploratoria/pokemon_alopez247.csv'
 ## Exercicio 2 - Analise individual
 
 def gen_hp_graph(pd_data):
-    pd_data['HP'].plot(kind='hist', title='HP', bins=range(0, 200, 10))
+    pd_data['HP'].plot(kind='box', title='HP', vert=False, xlim=(0, 200))
     plt.savefig('Analise Exploratoria/HP.png')
     plt.close()
     
 def gen_attack_graph(pd_data):
-    pd_data['Attack'].plot(kind='hist', title='Attack', bins=range(0, 200, 5))
+    pd_data['Attack'].plot(kind='box', title='Attack', vert=False, xlim=(0, 200))
     plt.savefig('Analise Exploratoria/Attack.png')
     plt.close()
 
 def gen_defense_graph(pd_data):
-    pd_data['Defense'].plot(kind='hist', title='Defense', bins=range(0, 200, 5))
+    pd_data['Defense'].plot(kind='box', title='Defense', vert=False, xlim=(0, 200))
     plt.savefig('Analise Exploratoria/Defense.png')
     plt.close()
 
 def gen_sp_attack_graph(pd_data):
-    pd_data['Sp_Atk'].plot(kind='hist', title='Sp. Attack', bins=range(0, 200, 5))
+    pd_data['Sp_Atk'].plot(kind='box', title='Sp. Attack', vert=False, xlim=(0, 200))
     plt.savefig('Analise Exploratoria/Sp_Attack.png')
     plt.close()
 
 def gen_sp_defense_graph(pd_data):
-    pd_data['Sp_Def'].plot(kind='hist', title='Sp. Defense', bins=range(0, 200, 5))
+    pd_data['Sp_Def'].plot(kind='box', title='Sp. Defense', vert=False, xlim=(0, 200))
     plt.savefig('Analise Exploratoria/Sp_Defense.png')
     plt.close()
 
 def gen_speed_graph(pd_data):
-    pd_data['Speed'].plot(kind='hist', title='Speed', bins=range(0, 200, 5))
+    pd_data['Speed'].plot(kind='box', title='Speed', vert=False, xlim=(0, 200))
     plt.savefig('Analise Exploratoria/Speed.png')
     plt.close()
 
 def gen_total_graph(pd_data):
-    pd_data['Total'].plot(kind='hist', title='Total', bins=range(0, 700, 10))
+    pd_data['Total'].plot(kind='box', title='Total', vert=False, xlim=(0, 800))
     plt.savefig('Analise Exploratoria/Total.png')
     plt.close()
 
@@ -163,32 +163,32 @@ if __name__ == '__main__':
 #### Ex 2
 
 
-    # gen_hp_graph(data_pandas)
-    # gen_attack_graph(data_pandas)
-    # gen_defense_graph(data_pandas)
-    # gen_sp_attack_graph(data_pandas)
-    # gen_sp_defense_graph(data_pandas)
-    # gen_speed_graph(data_pandas)
-    # gen_total_graph(data_pandas)
+    gen_hp_graph(data_pandas)
+    gen_attack_graph(data_pandas)
+    gen_defense_graph(data_pandas)
+    gen_sp_attack_graph(data_pandas)
+    gen_sp_defense_graph(data_pandas)
+    gen_speed_graph(data_pandas)
+    gen_total_graph(data_pandas)
 
-    # gen_weight_graph(data_pandas)
-    # gen_height_graph(data_pandas)
+    gen_weight_graph(data_pandas)
+    gen_height_graph(data_pandas)
 
-    # gen_no_pokemon_by_generation(data_pandas)
-    # gen_no_pokemon_by_first_type(data_pandas)
-    # gen_no_pokemon_by_second_type(data_pandas)
+    gen_no_pokemon_by_generation(data_pandas)
+    gen_no_pokemon_by_first_type(data_pandas)
+    gen_no_pokemon_by_second_type(data_pandas)
 
 #### Ex 3
 
-    # gen_weight_speed_comparison_graph(data_pandas)
-    # gen_weight_total_comparison_graph(data_pandas)
+    gen_weight_speed_comparison_graph(data_pandas)
+    gen_weight_total_comparison_graph(data_pandas)
 
-    # gen_height_speed_comparison_graph(data_pandas)
-    # gen_height_total_comparison_graph(data_pandas)
+    gen_height_speed_comparison_graph(data_pandas)
+    gen_height_total_comparison_graph(data_pandas)
 
-    # gen_avg_total_for_generation(data_pandas)
+    gen_avg_total_for_generation(data_pandas)
 
-    # gen_most_common_type_combination(data_pandas)
-    # gen_no_pokemon_by_type(data_pandas)
+    gen_most_common_type_combination(data_pandas)
+    gen_no_pokemon_by_type(data_pandas)
     gen_avg_total_by_type(data_pandas)
     get_avg_speed_by_type(data_pandas)
