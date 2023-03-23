145336cf87761f626fa9eb6d15b7317753e65a00
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2023-03-22 22:16:04 -0300

    start

---
+++ Predicao/main.py
@@ -0,0 +1,12 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+CSV_TRAIN_PATH = 'Predicao/treino.csv'
+CSV_TEST_PATH =  'Predicao/testes.csv'
+
+if __name__ == '__main__':
+    print("oi")
+
